a0083a2d2b1992ea8266ffdebe394a65909e2299
cuerojr <cannibal5033@gmail.com>
2026-05-06 01:00:58 -0300

    cc

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,6 @@
-import { defineConfig } from 'vite'
+import {
+  defineConfig
+} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
@@ -9,6 +11,26 @@ export default defineConfig({
     devSourcemap: false
   },
   build: {
-    assetsDir: 'admin_static', 
+    assetsDir: 'admin_static',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vendor-react': ['react', 'react-dom', 'react-router-dom'],
+          'vendor-redux': ['redux', 'react-redux', 'redux-saga', 'redux-thunk'],
+          'vendor-firebase': ['firebase'],
+          'vendor-editor': ['react-quill'],
+          'vendor-charts': ['chart.js', 'react-chartjs-2'],
+          'vendor-calendar': ['react-big-calendar', 'moment'],
+          'vendor-video': ['video.js'],
+          'vendor-xlsx': ['xlsx'],
+          'vendor-ui': ['reactstrap', 'react-select', 'react-datepicker'],
+        }
+      }
+    }
   },
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './src/setupTests.js',
+  }
 });
