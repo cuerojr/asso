dbed541783c2c2b35675a6da4c98771b52518826
cuerojr <cannibal5033@gmail.com>
2026-09-02 19:36:12 -0300

    asd

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/admin', // cambiar a /admin/ para produccion
+  base: '/', // cambiar a /admin/ para produccion
   css: {
     devSourcemap: false
   },
